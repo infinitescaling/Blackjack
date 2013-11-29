dbb4e54c39fb160da1f2843675e9a86b469fab1c
Scott <shogo564@gmail.com>
2013-11-29 13:58:20 -0500

    all logic is done, will add functions for re-initializing games when I care enough

--- blackjack.rb
+++ blackjack.rb
@@ -1,4 +1,10 @@
-deck = []
+def initialize(user_hand, deck, dealer_hand)
+    deck = populate_deck(deck)
+    user_hand = populate_user(user_hand, deck)
+    dealer_hand = populate_dealer(dealer_hand, deck)
+    puts "Game is initialized"
+end
+
 def populate_deck(deck)
     
     n = 2
@@ -16,24 +22,51 @@ def populate_deck(deck)
         deck << 'Q'
         deck << 'K'
     end
-end
 
-populate_deck(deck)
-user_hand = []
+    deck.shuffle!
+        return deck
+end
 
 def populate_user(user_hand, deck)
    
     2.times do
             user_hand << deck.delete_at(rand(deck.length))
     end
+    return user_hand
 end
 
-populate_user(user_hand, deck)
+def calculate_value(hand)
+    hand_value = 0
 
-deck.shuffle!
-puts "User hand: "
-print user_hand
-puts 
+    hand.each do |x|
+        if ['2','3','4','5','6','7','8','9'].include? x
+            hand_value+= x.to_i
+        elsif ['J', 'Q', 'K'].include? x
+            hand_value =  hand_value + 10
+        elsif x == "A"
+            if(hand_value + 11) > 21
+                hand_value+= 1
+            else
+                hand_value+= 11
+            end
+        end
+    end
+
+    return hand_value
+end
+
+def populate_dealer(dealer_hand, deck)
+    2.times do
+        dealer_hand << deck.delete_at(rand(deck.length))
+    end
+    return dealer_hand
+end
+
+#start game
+deck = []
+user_hand = []
+dealer_hand = []
+initialize(user_hand, deck, dealer_hand)
 
 while(true) do
 
@@ -41,18 +74,86 @@ while(true) do
     #I thought this up myself I'm so smart.
     user_hand.sort!{|x,y| x.ord <=> y.ord}
 
-    puts user_hand
+    
+    #calculate user hand value
+    calculate_value(user_hand)
+    hand_value = calculate_value(user_hand)
+    
+    #calculate dealer hand value
+    dealer_value = calculate_value(dealer_hand)
+
+    #print out user_hand
+    puts "User hand: #{user_hand}"
+
+    #print out user's hand value
+    puts "Your hand is #{hand_value}"
+
+    if(dealer_value == 21)
+        #print out dealer's full cards
+        puts "Dealer hand #{dealer_hand}"
+        puts dealer_value
+    else
+        #print out dealer's first card
+        puts "Dealer hand " + dealer_hand[1]
+        temp = []
+        temp << dealer_hand[1]
+        temp_value = calculate_value(temp)
+        puts "Dealer hand value: #{temp_value}"
+    end
+
+
+
+    if hand_value > 21
+        puts "BUST! Play again? Y/N"
+        user_input = gets.chomp!
+        user_input.upcase!
+        case user_input
+            when "Y"
+                deck = []
+                user_hand = []
+                dealer_hand = []
+                initialize(user_hand, deck, dealer_hand)
+                calculate_value(user_hand)
+                hand_value = calculate_value(user_hand)
+                puts "Your hand is #{user_hand}"
+                puts "Your hand is #{hand_value}"
+            when "N"
+                abort("Thank you for playing")
+            else
+                puts "Incorrect input"
+            end
+    end
 
     puts "Hit, Stay, or Abort"
-    user_input = gets.chomp
+    user_input = gets.chomp!
+    user_input.downcase!    
 
     case user_input
     when "hit"
-
+        #draw from deck, recalculate values
+        user_hand << deck.delete_at(rand(deck.length))
     when "stay"
 
+        puts dealer_value
+        while(true)
+            if(dealer_value >= hand_value)
+                puts "Dealer wins with a value of #{dealer_value} > #{hand_value}"
+                break
+            else
+                while(dealer_value < 15 || dealer_value < hand_value)
+                    dealer_hand << deck.delete_at(rand(deck.length))
+                    dealer_value = calculate_value(dealer_hand)
+                    #puts "Dealer value is now #{dealer_value}"
+                end
+                if dealer_value > 21 || hand_value > dealer_value
+                    puts "You win!"
+                    break
+                end
+            end
+        end
     when "abort"
-
+        #end program
+        abort("Thank you for playing!")
     else
         puts "Incorrect input"
     end
